24b03b83c8226179fcd7d5a0db684b930734f5a3
soola-sys <agdarbekovsultan@gmail.com>
2025-01-19 22:14:07 +0600

    feat: configure eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import './style.css';
-function component (text: string) {
-    const element = document.createElement('p');
-    element.textContent = text;
-    element.classList.add('test-class');
-    return element;
+function component(text: string) {
+  const element = document.createElement("p");
+  element.textContent = text;
+  element.classList.add('test-class');
+  return element;
 }
 document.body.append(component('Hello'));
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,48 +1,54 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import EslintPlugin from 'eslint-webpack-plugin';
 
 interface IEnv {
-    mode: 'production' | 'development'
+  mode: 'production' | 'development';
 }
 
-export default (env : IEnv) => {
-    const config: webpack.Configuration = {
-        mode: env.mode ?? 'production',
-        entry: path.resolve(__dirname, 'src' , 'index.ts'),
-        output: {
-          filename: '[name].js',
-          path: path.resolve(__dirname, 'build'),
-          clean: true,
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: ['style-loader', 'css-loader'],
-                },
-                {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-            ],
-        },
-        resolve: {
-            extensions: [".tsx", ".ts", ".js"],
-        },
-        devServer:  {
-            static: {
-                directory: path.join(__dirname, './build')
-            },
-        },        
-        plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src' ,'index.html'),
-            favicon: "./src/components/view/img/css-icon.png"
-        })
-    ],
-    }
-    return config
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
 }
+
+export default (env: IEnv) => {
+  const config: Configuration = {
+    mode: env.mode ?? 'production',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, './build'),
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src', 'index.html'),
+        favicon: './src/components/view/img/css-icon.png',
+      }),
+      new EslintPlugin({ extensions: ['ts'] }),
+    ],
+  };
+  return config;
+};
