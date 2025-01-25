3b5d4708db22c1864cb3c1a40fba2083ba437410
soola-sys <agdarbekovsultan@gmail.com>
2025-01-25 20:31:12 +0600

    feat: start basic layout

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
 import './style.css';
-function component(text: string) {
-  const element = document.createElement("p");
-  element.textContent = text;
-  element.classList.add('test-class');
-  return element;
-}
-document.body.append(component('Hello'));
